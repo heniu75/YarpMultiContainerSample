87964f60dcf1b581d8cd875e4d68904117cf3c10
Heinrich van Vught <heinrich.van.vught@gmail.com>
2024-10-19 20:27:06 +0100

    Dot net core multicontainer solution with YARP

---
+++ MyYarpMock/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyYarpMock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var configSectionName = GetYarpConfigSectionName();
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services
+                .AddReverseProxy()
+                .LoadFromConfig(builder.Configuration.GetSection(configSectionName));
+
+            var app = builder.Build();
+
+            app.MapGet("/yarp", () =>
+            {
+                var x = 10;
+                return $"Yarp time is {DateTime.Now}";
+            });
+
+            app.MapReverseProxy();
+
+            app.Run();
+        }
+
+        private static string GetYarpConfigSectionName()
+        {
+            var docker_env = Environment.GetEnvironmentVariable("DOCKER_ENV");
+            var yarpConfig = docker_env == "true"
+                ? "ReverseProxyDocker"
+                : "DockerReverseProxy";
+            return yarpConfig;
+        }
+    }
+}
